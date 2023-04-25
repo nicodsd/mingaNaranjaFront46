52763962db4d75ed74e01a92c80fb72f8298c2ca
Nico Barrera <57334167+nicodsd@users.noreply.github.com>
2023-04-25 10:57:59 -0300

    F 02 (#4)
    * f-02
    
    * f-02

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,25 +1,32 @@
+import footeR from '../assets/images/Footer.png'
+import logo from '../assets/images/logos/Logo21.png'
+import facebook from '../assets/images/icons/Facebook.png'
+import twitter from '../assets/images/icons/Twitter.png'
+import youtube from '../assets/images/icons/Youtube.png'
+import vimeo from '../assets/images/icons/Vimeo.png'
+import heart from '../assets/images/icons/Union.png'
 
 export default function Footer() {
     return (
         <footer className='h-[15rem] md:h-[19rem] lg:h-[39rem] w-full relative'>
-            <img className='rounded-b-[50%] w-full' src="./src/assets/images/Footer.png" alt="" />
+            <img className='rounded-b-[50%] w-full' src={footeR} alt="" />
             <div className='flex justify-center items-center h-[3rem] lg:h-[12rem]'>
                 <div className='w-full mt-8 lg:w-[60rem] lg:mt-[6rem] flex items-center justify-around lg:justify-between lg:border-b-[1px] lg:border-[#ffffff40]'>
                     <div className='text-white text-[10px] w-[6rem] lg:w-[9rem] flex justify-between'>
                         <a className='font-semibold' href="_blank">Home</a>
                         <a className='font-semibold' href="_blank">Mangas</a>
                     </div>
-                    <img className='h-[1.5rem] lg:h-12' src="./src/assets/images/logos/logo21.png" alt="" />
+                    <img className='h-[1.5rem] lg:h-12' src={logo} alt="" />
                     <div className='h-[3.5rem] lg:h-[6.5rem] flex flex-col justify-around'>
                         <div className='w-full gap-1 lg:w-[13rem] flex justify-center lg:gap-[2.3rem]'>
-                            <img className='w-[18px] h-[18px] lg:w-[1.4rem] lg:h-[1.4rem]' src="./src/assets/images/icons/Facebook.png" alt="" />
-                            <img className='w-[22px] h-[18px] lg:w-[30px] lg:h-[1.2rem]' src="./src/assets/images/icons/Twitter.png" alt="" />
-                            <img className='w-[22px] h-[18px] lg:w-[30px] lg:h-[1.2rem]' src="./src/assets/images/icons/Vimeo.png" alt="" />
-                            <img className='w-[22px] h-[18px] lg:w-[35px] lg:h-[1.2rem]' src="./src/assets/images/icons/Youtube.png " alt="" />
+                            <img className='w-[18px] h-[18px] lg:w-[1.4rem] lg:h-[1.4rem]' src={facebook} alt="" />
+                            <img className='w-[22px] h-[18px] lg:w-[30px] lg:h-[1.2rem]' src={twitter} alt="" />
+                            <img className='w-[22px] h-[18px] lg:w-[30px] lg:h-[1.2rem]' src={vimeo} alt="" />
+                            <img className='w-[22px] h-[18px] lg:w-[35px] lg:h-[1.2rem]' src={youtube} alt="" />
                         </div>
                         <button className='bg-white w-full h-[1.5rem] lg:w-[13rem] lg:h-[2.6rem] rounded-[5px] flex justify-center items-center'>
                             <p className='font-semibold text-[4px] lg:text-[15px]'>Donate</p>
-                            <img className='h-[13px] ml-1 lg:ml-4' src="./src/assets/images/icons/Union.png" alt="" />
+                            <img className='h-[13px] ml-1 lg:ml-4' src={heart} alt="" />
                         </button>
                     </div>
                 </div>
