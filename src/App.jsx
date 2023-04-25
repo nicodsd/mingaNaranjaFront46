e72f63e43b33c0baebde608e21ff45cd1800b3a9
Nico Barrera <57334167+nicodsd@users.noreply.github.com>
2023-04-25 04:50:19 -0300

    m-02 (#2)

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
+import apiUrl from '../api'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
 import './App.css'
 
 function App() {
-  /*     background-image: linear-gradient(to right, #000000, #ffffff4a, #000000);
-      background-position: bottom;
-      background-size: 100% 1px;
-      background-repeat: no-repeat; */
+  console.log(apiUrl)
+  console.log(process.env.NODE_ENV)
   return (
     <>
       <div className='bg-black w-full h-full'>
 
-        <nav className='p-5 flex justify-between items-center mb-4 lg:mb-0'>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="w-8 h-10">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-          </svg>
-          <img src="./src/assets/images/logos/logo21.png" alt="logoMinga" />
-        </nav>
+      <Navbar />
 
         <div className='lg:w-full lg:h-[1px]' style={{ backgroundImage: 'linear-gradient(to right, #000, #ffffff40, #000)' }}></div>
 
@@ -39,30 +35,7 @@ function App() {
           </div>
         </main>
 
-        <footer className='h-[15rem] md:h-[19rem] lg:h-[39rem] w-full relative'>
-          <img className='rounded-b-[50%] w-full' src="./src/assets/images/cswc5oyv1.png" alt="" />
-          <div className='flex justify-center items-center h-[3rem] lg:h-[12rem]'>
-            <div className='w-full mt-8 lg:w-[60rem] lg:mt-[6rem] flex items-center justify-around lg:justify-between lg:border-b-[1px] lg:border-[#ffffff40]'>
-              <div className='text-white text-[10px] w-[6rem] lg:w-[9rem] flex justify-between'>
-                <a className='font-semibold' href="_blank">Home</a>
-                <a className='font-semibold' href="_blank">Mangas</a>
-              </div>
-              <img className='h-[1.5rem] lg:h-12' src="./src/assets/images/logos/logo21.png" alt="" />
-              <div className='h-[3.5rem] lg:h-[6.5rem] flex flex-col justify-around'>
-                <div className='w-full gap-1 lg:w-[13rem] flex justify-center lg:gap-[2.3rem]'>
-                  <img className='w-[18px] h-[18px] lg:w-[1.4rem] lg:h-[1.4rem]' src="./src/assets/images/icons/Facebook.png" alt="" />
-                  <img className='w-[22px] h-[18px] lg:w-[30px] lg:h-[1.2rem]' src="./src/assets/images/icons/Twitter.png" alt="" />
-                  <img className='w-[22px] h-[18px] lg:w-[30px] lg:h-[1.2rem]' src="./src/assets/images/icons/Vimeo.png" alt="" />
-                  <img className='w-[22px] h-[18px] lg:w-[35px] lg:h-[1.2rem]' src="./src/assets/images/icons/Youtube.png" alt="" />
-                </div>
-                <button className='bg-white w-full h-[1.5rem] lg:w-[13rem] lg:h-[2.6rem] rounded-[5px] flex justify-center items-center'>
-                  <p className='font-semibold text-[4px] lg:text-[15px]'>Donate</p>
-                  <img className='h-[13px] ml-1 lg:ml-4' src="./src/assets/images/icons/Union.png" alt="" />
-                </button>
-              </div>
-            </div>
-          </div>
-        </footer>
+    <Footer />
 
       </div>
     </>
