2cff585e98b1310c6aa8843b53209ad54319b948
Nicolas <nilonb7@gmail.com>
2023-04-26 12:25:05 -0300

    f-05

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,18 @@
+import Sidebar from './Sidebar'
 import logo from '../assets/images/logos/Logo21.png'
+import { FiMenu } from "react-icons/fi"
+import '../../src/App.css'
+
+
 export default function Navbar() {
+
     return (
-        <nav className='p-5 flex justify-between items-center mb-4 lg:mb-0'>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="w-8 h-10">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-            </svg>
-            <img src={logo} alt="logoMinga" />
+        <nav className='p-5 lg:px-10 flex justify-between items-center mb-4 lg:mb-0'>
+            <div className='bars'>
+                <FiMenu />
+            </div>    
+            <Sidebar />
+            <img src={logo} className='w-[5rem]' alt="logoMinga" />
         </nav>
     )
-}
+    }
